a0a7b7446a494e6691a81007a2390e3dcd154aae
Yiring Yeung <yiring.yeung@kuehne-nagel.com>
2020-02-05 17:19:00 +0100

    User class added

---
+++ src/main/java/User.java
@@ -0,0 +1,17 @@
+public class User {
+    private String userName;
+    private String password;
+
+    public User(String userName, String password){
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
