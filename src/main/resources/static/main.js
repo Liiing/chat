daac71366a6f689a9c859d7731bcab7259c40f6a
Yiring Yeung <yiring.yeung@kuehne-nagel.com>
2020-02-07 12:21:49 +0100

    Changed html: outsourced register and login in divs implemented js functions by onkeypress validation functions were added

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,16 +1,10 @@
 let connection = new WebSocket('ws://localhost:4444');
-let textBox = document.getElementById("textBox");
-let chatBox = document.getElementById("chatBox");
-let username = document.getElementById("username");
-let password = document.getElementById("password");
-
+let newUserPasswordsCorrect;
+let newUsername;
+let strongPassword;
 
 connection.onopen = function () {
     log('Websocket open!');
-
-    //sende ein hallo nach dem verbinden
-    //connection.send("Hallo")
-
 };
 
 connection.onerror = function (error) {
@@ -23,12 +17,55 @@ connection.onmessage = function (messageEvent) {
     sendToChatBox(messageAsString);
 };
 
+function showRegisterElements() {
+    document.getElementById("loginMask").hidden=true;
+    document.getElementById("registerMask").hidden=false;
+}
+
+function checkNewUsername() {
+    let username = document.getElementById("username");
+
+}
+
+function checkNewUserPasswordStrength() {
+    let passwordInformation = document.getElementById("passwordInformation");
+    let password = document.getElementById("password").value;
+    let strength = 0;
+
+    if (password.match(/[a-z]+/)) {
+        strength += 1;
+    }
+    if (password.match(/[A-Z]+/)) {
+        strength += 1;
+    }
+    if (password.match(/[0-9]+/)) {
+        strength += 1;
+    }
+    if (password.match(/[$@#&!]+/)) {
+        strength += 1;
+    }
+
+    if (strength == 6){
+        strongPassword = true;
+    }
+}
+
+function compareNewUserPasswords() {
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (password == confirmPassword) {
+        newUserPasswordsCorrect = true;
+    }
+}
+
 function registerButtonClick() {
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
     let user = new Object();
+
     user.username = username.value;
     user.password = password.value;
-    //let user = username.value;
-    //let password = password.value;
 
     registerUser(user);
 }
@@ -44,10 +81,12 @@ function registerUser(user) {
 }
 
 function sendToChatBox(message) {
+    let chatBox = document.getElementById("chatBox");
     chatBox.innerHTML = chatBox.innerHTML + message;
 }
 
 function messageButtonClick() {
+    let textBox = document.getElementById("textBox");
     let message = textBox.value;
 
     send('message=' + message);
